72b70ac811ab1707dddda839f5c3f4891a34f125
Andrew Chao <dev@silvermice.com>
2024-05-24 20:29:28 +0000

    refactor to remove selenium, swap to weasyprint (headless)

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,11 @@ WORKDIR /usr/src/app
 RUN apt update
 RUN apt install graphicsmagick -y
 RUN apt install python3 python3-pip -y
+RUN apt install libpango-1.0-0 -y
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD gunicorn main:app --bind=0.0.0.0:8000 --workers=4
+CMD gunicorn main:app --bind=0.0.0.0:7423 --workers=2
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import render
+# import render
 from flask import Flask, render_template, send_file
 app = Flask("firewood")
 
@@ -8,14 +8,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/image")
-def image():
-    generate_image("index.html")
-    return send_file('tmp/output.png', mimetype='image/gif')
+#@app.route("/image")
+#def image():
+    # generate_image("index.html")
+    # return send_file('tmp/output.png', mimetype='image/gif')
 
 
 def generate_image(page):
-    render.screenshot() # this will occupy a worker!
+    # render.screenshot() # this will occupy a worker!
     # we use conversion to drop from three channels to a single 8-bit channel
     os.system("gm convert tmp/source.png tmp/lossy.jpg")
     os.system("gm convert tmp/lossy.jpg -colorspace Gray -resize 600x800 tmp/output.png")
