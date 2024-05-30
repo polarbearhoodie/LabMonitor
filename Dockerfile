b26b3973b4c4589a7b3434e1116ce21ef7b52d1f
Andrew Chao <dev@silvermice.com>
2024-05-30 01:01:56 -0700

    refactoring

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM ubuntu:jammy
 WORKDIR /usr/src/app
 
 RUN apt update
-RUN apt install graphicsmagick -y
 RUN apt install python3 python3-pip -y
-RUN apt install libpango-1.0-0 -y
+RUN apt install graphicsmagick -y
+RUN apt install weasyprint
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD gunicorn main:app --bind=0.0.0.0:7423 --workers=1
+CMD gunicorn main:app --bind=0.0.0.0:8000 --workers=2
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import os
-from flask import Flask, render_template, send_file
+import datetime
+import calendar
 
-app = Flask("Monitor")
+from flask import Flask, send_file
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return get_current_time()
 
 
 @app.route("/image")
 def image():
-    # generate_image("index.html")
-    # return send_file('tmp/output.png', mimetype='image/gif')
+    return send_file('tmp/output.png', mimetype='image/gif')
 
 
 def generate_image(page):
-    # render.screenshot() # this will occupy a worker!
     # we use conversion to drop from three channels to a single 8-bit channel
     os.system("gm convert tmp/source.png tmp/lossy.jpg")
     os.system("gm convert tmp/lossy.jpg -colorspace Gray -resize 600x800 tmp/output.png")
+
+# returns the current time as a formatted string
+# the string is HR:MIN AM/PM - DAY, MONTH #, 20xx
+def get_current_time():
+    today = datetime.datetime.today()
+
+    hour = today.hour
+    minute = today.minute
+
+    if hour > 12:
+        state = "AM"
+        hour -= 12
+    else:
+        state = "PM"
+
+    day_name = calendar.day_name[today.weekday()]
+    month_name = calendar.month_name[today.month]
+    
+    day_number = today.day
+    year_number = today.year
+
+    return "{0}:{1} {2} - {3}, {4} {5}, {6}".format(hour, minute, state, day_name, month_name, day_number, year_number)
+
+print(get_current_time())
